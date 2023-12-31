5967d9a66b02362af9fbbb7fa074898158910a56
Can Cekic <ac.cekic@outlook.com>
2023-12-31 23:28:03 +0100

    Reorganizing repository

--- src/Program.java
+++
@@ -1,9 +0,0 @@
-public class Program {
-	public static void main(String[] args) {
-		Cat moneypenny = new Cat("Moneypenny");
-		Cat noodle = new Cat("Noodle");
-
-		moneypenny.meow();
-		noodle.meow();
-	}
-}
