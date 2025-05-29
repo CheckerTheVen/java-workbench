80e4d53f2d1fca009ca892a20a49409d3378b500
CheckerTheVen <ac.cekic@outlook.com>
2025-05-29 23:38:51 +0200

    Updated: Testing with Java `stream`s

---
+++ src/checker/streams/Cat.java
@@ -0,0 +1,6 @@
+package checker.streams;
+
+import java.time.LocalDate;
+
+public record Cat(String name, LocalDate birth, double weight, Gender gender) { }
+
---
+++ src/checker/streams/Gender.java
@@ -0,0 +1,3 @@
+package checker.streams;
+
+public enum Gender { MALE, FEMALE, UNOBSERVED }
